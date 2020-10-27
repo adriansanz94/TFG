ce26b3695480d08e4bc6365f784f5bcc1de7bfd4
Steven-Cadena <steven@gmail.com>
2020-10-27 21:13:53 +0100

    ya estan las tablas del admin y funcionan el botón de borrar

--- resources/templates/contenido/admin.php
+++ resources/templates/contenido/admin.php
@@ -15,10 +15,11 @@
 
        $usuarios = UsuarioManager::getAll();
        $recetas = RecetaManager::getAll();
-       $rutinas = RecetaManager::getAll();
-       $ejercicios = RecetaManager::getAll();
+       $ejercicios = EjercicioManager::getAll();
+       $rutinas = RutinaManager::getAll();
 
-       echo '<pre>';
+
+     /*echo '<pre>';
      print_r($usuarios);
      echo '</pre>';
      echo '<pre>';
@@ -29,7 +30,7 @@
      echo '</pre>';
      echo '<pre>';
      print_r($ejercicios);
-     echo '</pre>';
+     echo '</pre>';*/
 
 
      //SINO AL INICIO
@@ -41,5 +42,150 @@
     // header('Location: principal.php');
      exit;
    }
-
 ?>
+<style media="screen">
+  img{
+    background-color: white;
+    width: 70px;
+    height: 75px;
+  }
+</style>
+
+<div> <h2>Usuarios</h2>
+  <div>
+    <div class="usuarios">
+  <table border="1px">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Contraseña</th>
+        <th>Email</th>
+        <th>Imagen</th>
+        <th>Rol</th>
+        <th>Borrar Usuario</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php foreach ($usuarios as $fila) { ?>
+    <tr>
+      <!--<td>
+        <a href="/perfilPublico.php?id_user=<?=$fila['ID']?>">
+          <img id=fotoPerfilUser src="imgs/<?=$fila['ID']?>/<?=$fila['foto']?>" alt="">
+        </a>
+      </td>-->
+      <td><?=$fila['ID']?></td>
+      <td><?=$fila['NOMBRE']?></td>
+      <td><?=$fila['PASS']?></td>
+      <td><?=$fila['EMAIL']?></td>
+      <td><?=$fila['IMAGEN']?></td>
+      <td><?=$fila['ROL']?></td>
+      <td>
+        <a href="borrarADMIN.php?id_usuario=<?=$fila['ID']?>"><img id="basura" src="papelera.png" alt="Borrar usuario"></a>
+      </td>
+    </tr>
+  <?php } ?>
+  </tbody>
+  </table>
+  </div>
+</div>
+
+
+<div> <h2>Recetas</h2>
+  <div>
+    <div class="recetas">
+  <table border="1px">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Descripcion</th>
+        <th>Tiempo</th>
+        <th>Imagen</th>
+        <th>Id_usuario</th>
+        <th>Borrar Receta</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php foreach ($recetas as $fila) { ?>
+    <tr>
+      <td><?=$fila['ID']?></td>
+      <td><?=$fila['NOMBRE']?></td>
+      <td><?=$fila['DESCRIPCION']?></td>
+      <td><?=$fila['TIEMPO']?></td>
+      <td><?=$fila['IMAGEN']?></td>
+      <td><?=$fila['ID_USUARIO']?></td>
+      <td>
+        <a href="borrarADMIN.php?id_receta=<?=$fila['ID']?>"><img id="basura" src="imagenes/papelera.png" alt="Borrar usuario"></a>
+      </td>
+    </tr>
+  <?php } ?>
+  </tbody>
+  </table>
+  </div>
+</div>
+
+
+<div> <h2>Ejercicios</h2>
+  <div>
+    <div class="ejercicios">
+  <table border="1px">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Grupo Muscular</th>
+        <th>Descripcion</th>
+        <th>Imagen</th>
+        <th>Borrar Receta</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php foreach ($ejercicios as $fila) { ?>
+    <tr>
+      <td><?=$fila['ID']?></td>
+      <td><?=$fila['NOMBRE']?></td>
+      <td><?=$fila['GRUPOMUSCULAR']?></td>
+      <td><?=$fila['DESCRIPCION']?></td>
+      <td><?=$fila['IMAGEN']?></td>
+      <td>
+        <a href="borrarADMIN.php?id_ejercicio=<?=$fila['ID']?>"><img id="basura" src="imagenes/papelera.png" alt="Borrar usuario"></a>
+      </td>
+    </tr>
+  <?php } ?>
+  </tbody>
+  </table>
+  </div>
+</div>
+
+
+
+<div> <h2>Rutinas</h2>
+  <div>
+    <div class="rutinas">
+  <table border="1px">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>ID_Rutina</th>
+        <th>ID_Ejercicio</th>
+        <th>Repetición</th>
+        <th>Borrar Rutina</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php foreach ($rutinas as $fila) { ?>
+    <tr>
+      <td><?=$fila['ID']?></td>
+      <td><?=$fila['NOMBRE']?></td>
+      <td><?=$fila['DIFICULTAD']?></td>
+      <td><?=$fila['DESCRIPCION']?></td>
+      <td>
+        <a href="borrarADMIN.php?id_rutina=<?=$fila['ID']?>"><img id="basura" src="imagenes/papelera.png" alt="Borrar usuario"></a>
+      </td>
+    </tr>
+  <?php } ?>
+  </tbody>
+  </table>
+  </div>
+</div>
---
+++ resources/templates/contenido/borrarADMIN.php
@@ -0,0 +1,31 @@
+<?php
+
+$id = "";
+
+if(isset($_GET['id_usuario'])){
+  $id = $_GET['id_usuario'];
+  UsuarioManager::delete($id);
+
+}
+
+if(isset($_GET['id_receta'])){
+  $id = $_GET['id_receta'];
+  RecetaManager::delete($id);
+}
+
+if(isset($_GET['id_ejercicio'])){
+  $id = $_GET['id_ejercicio'];
+  EjercicioManager::delete($id);
+}
+
+if(isset($_GET['id_rutina'])){
+  $id = $_GET['id_rutina'];
+  RutinaManager::delete($id);
+}
+
+header("Location:admin.php");
+die();
+
+
+
+?>
