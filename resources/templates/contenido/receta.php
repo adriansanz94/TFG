d157f694fb319d2d3472667d48e623cfe7c6a969
adriansanz94 <adriansanzperez.94@gmail.com>
2020-11-16 18:31:37 +0100

    quitado  enlace a perfil en receta

--- resources/templates/contenido/receta.php
+++ resources/templates/contenido/receta.php
@@ -53,7 +53,7 @@ for ($i=0; $i < count($datosReceta['INGREDIENTES']); $i++) {
       <?php   } ?>
        </div>
   </div>
-  <a href="">Receta subida por:<?=$recetaSubida['']?> </a>
+
 
 </div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
