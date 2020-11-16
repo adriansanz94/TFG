05168d9a8238097101c905a4e35395cb947743de
adriansanz94 <adriansanzperez.94@gmail.com>
2020-11-17 00:31:48 +0100

    archivo quienes somos .php

---
+++ resources/templates/contenido/quienesSomos.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<div class="quienesSomos">
+  <h2>Ponte En Forma</h2>
+  <div class='texto'>
+    <p>
+      Esta empresa ha sido creada por Adrián Sanz, Steven cadena y Gérman Romero, alumnos de DAW del IES Juan de la cierva.
+    </p>
+    <p>
+      Nuestra empresa, llamada 'PONTE EN FORMA', es una web, donde puedes buscar y subir tanto rutinas como recetas.En esta web la gente podra subir sus rutinas favoritas,
+      para compartirlas con todo el mundo, asi como sus platos saludables y lo ricos que estan para que todos puedan hacerlos.
+    </p>
+    <p>
+      Nuesta Web quiere acercar el mundo del deporte y la comida sana a todo el mundo que quiere empezar y no puede permitirse un entrenador o un gimnasio, o simplemente dispone del tiempo suficiente 
+      para poder entrenar, ademas de aportar ideas sobre comidas saludables faciles de hacer y paso a paso, para que no tengan perdida a la hora de hacerlas.
+    </p>
+    <p>
+      En esta web da igual si eres un pro del deporte, un novato que quiere empezar o llevas toda la vida compitiendo a nivel profesional, aqui hay sitio para todo elmundo que quiera llevar una vida mas sana.
+    </p>
+    <p>
+      A día de hoy hay muchas paginas que prometen milagros o resultados rapidos, pero nosotros sabemos que todo necesita de tiempo y esfuerzo, por eso la idea de acercaros a todos vosotros, gente como nostoros, la posibilidad de dar un cambio a vuestras vidas.
+      A si que si estas dispuesto a dar un cambio en tu vida o simplemente buscas ideas nuevas para que tus entrenamientos y dietas sean algo diferentes este es tu sitio.
+    </p>
+    <br>
+    <br>
+    <p>Queremos agradecer tambien a nuestros profesores de primero y segundo año, sin los cuales todo esto no habria sido posible.  </p>
+    <br>
+    <p>
+      Proyecto creado por Adrián Sanz, Steven cadena y Gérman Romero alumnos de DAW del Instituto Juan de la Cierva.
+    </p>
+    <br>
+    <p>2020</p>
+  </div>
+
+
+</div>
