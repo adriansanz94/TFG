18c496ac739e6403ce80bffff1366029903628fc
Steven-Cadena <steven@gmail.com>
2020-11-18 00:33:02 +0100

    arreglado quienes somos

--- resources/templates/contenido/quienesSomos.php
+++ resources/templates/contenido/quienesSomos.php
@@ -5,29 +5,33 @@
   <h2>Ponte En Forma</h2>
   <div class='texto'>
     <p>
-      Esta empresa ha sido creada por Adrián Sanz, Steven cadena y Gérman Romero, alumnos de DAW del IES Juan de la cierva.
+      Esta empresa ha sido fundada por Adrián Sanz, Steven Cadena y Gérman Romero, alumnos de DAW del IES Juan de la Cierva.
     </p>
     <p>
-      Nuestra empresa, <strong> 'PONTE EN FORMA'</strong>, es una web, donde puedes buscar y subir tanto rutinas como recetas.En esta web la gente podra subir sus rutinas favoritas,
-      para compartirlas con todo el mundo, asi como sus platos saludables y lo ricos que estan para que todos puedan hacerlos.
+      Nuestra empresa, <strong> 'PONTE EN FORMA'</strong>, es una web donde puedes buscar y subir tanto rutinas como recetas. En esta página la gente podra subir sus rutinas favoritas,
+      para compartirlas con todo el mundo, así como sus platos saludables y deliciosos, para que todos puedan disfrutarlos.
     </p>
     <p>
-      Nuesta Web quiere acercar el mundo del deporte y la comida sana a todo el mundo que quiere empezar y no puede permitirse un entrenador o un gimnasio, o simplemente dispone del tiempo suficiente 
-      para poder entrenar, ademas de aportar ideas sobre comidas saludables faciles de hacer y paso a paso, para que no tengan perdida a la hora de hacerlas.
+      Nuesta página quiere acercar el mundo del deporte y la comida sana a todos aquellos que ya disfruten de una vida saludable o quieran empezar a llevarla. Es especialmente apta para quienes no pueden permitirse
+      un entrenador o un gimnasio, pero puede acceder todo el que desee, ¡sin restricciones! Además, existe la opcion de aportar ideas de comidas saludables, de todo tipo de niveles de complicación y, eso sí,
+      paso a paso, para que no tengan pérdida a la hora de llevarlas a cabo.
     </p>
     <p>
-      En esta web da igual si eres un pro del deporte, un novato que quiere empezar o llevas toda la vida compitiendo a nivel profesional, aqui hay sitio para todo elmundo que quiera llevar una vida mas sana.
+      A día de hoy, te encontrarás con infinitas páginas que prometan resultados rápidos y sin esfuerzos. Pero seamos realistas, nada es posible sin poner dedicación. Así que si estás dispuesto a dar un cambio en tu vida,
+      o simplemente buscas ideas nuevas para que tus entrenamientos y dietas sean algo diferentes, este es tu sitio.
+
     </p>
     <p>
-      A día de hoy hay muchas paginas que prometen milagros o resultados rapidos, pero nosotros sabemos que todo necesita de tiempo y esfuerzo, por eso la idea de acercaros a todos vosotros, gente como nostoros, la posibilidad de dar un cambio a vuestras vidas.
-      A si que si estas dispuesto a dar un cambio en tu vida o simplemente buscas ideas nuevas para que tus entrenamientos y dietas sean algo diferentes este es tu sitio.
+      En esta web da igual si eres un pro del deporte, un novato que quiere empezar o llevas toda la vida compitiendo, aquí hay sitio para todo el mundo. Así que ya sabes, si lo que quieres es llevar una vida mas sana,
+      ¡no dudes en registrarte!
+
     </p>
-    
+
     <br>
-    <p>Queremos agradecer tambien a nuestros profesores de primero y segundo año, sin los cuales todo esto no habria sido posible.  </p>
+    <p>Queremos agradecer también a nuestros profesores del primer y segundo año, sin los cuales todo esto no habría sido posible.  </p>
 
     <p>
-      Proyecto creado por Adrián Sanz, Steven cadena y Gérman Romero alumnos de DAW del Instituto Juan de la Cierva.
+      Proyecto creado por Adrián Sanz, Steven Cadena y Gérman Romero, alumnos de DAW del Instituto Juan de la Cierva.
     </p>
     <br>
     <p>DAW 2020</p>
