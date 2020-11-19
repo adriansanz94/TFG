d48a2ba9a260656d4d69d39f3e86e2df170f3242
Steven-Cadena <steven@gmail.com>
2020-11-19 19:21:10 +0100

    arreglado botón subir

--- resources/templates/contenido/principal.php
+++ resources/templates/contenido/principal.php
@@ -50,16 +50,8 @@ for ($i=0; $i < count($datosReceta); $i++) {
       <div id="receta" class="receta" data-id="<?=$datosReceta[$i]['ID']?> ">
       <h2><a href="receta.php?id=<?= $datosReceta[$i]['ID']?>"><?= $datosReceta[$i]['NOMBRE']?></a></h2>
       <figure><img src="<?=$datosReceta[$i]['IMAGEN'] ?>"></figure>
-      <!--<p class="negrita">Descripción:</p>-->
-      <!--<p><?= $datosReceta[$i]['DESCRIPCION']?></p>-->
       <p class="negrita">Tiempo:</p>
       <p><?= $datosReceta[$i]['TIEMPO']?></p>
-      <!--<p class="negrita">Ingredientes:</p>
-
-      <?php  for ($k=0; $k < count($ingredientesSolos[$i]); $k++) { ?>
-              <p> - <?=$ingredientesSolos[$i][$k]?></p>
-
-      <?php   } ?>-->
       </div>
     <?php } ?>
   </div>
@@ -107,29 +99,21 @@ for ($i=0; $i < count($datosReceta); $i++) {
     let a = crearElemento('a',{href:'receta.php?id='+ recetaJSON.ID},recetaJSON.NOMBRE);
     let figure = crearElemento('figure',null,null);
     let img = crearElemento('img',{src:recetaJSON.IMAGEN},null);
-    /*let pDescripcion = crearElemento('p',null,recetaJSON.DESCRIPCION);
-    let pTituloDescripcion = crearElemento('p',{'class':'negrita'},'Descripción: ');*/
     let pTituloTiempo = crearElemento('p',{'class':'negrita'},'Tiempo: ');
     let pTiempo = crearElemento('p',null,recetaJSON.TIEMPO);
-    //let pTituloIngredientes = crearElemento('p',null,'Ingredientes:');
+
 
     h2.appendChild(a);
     figure.appendChild(img);
     divReceta.appendChild(h2);
     divReceta.appendChild(figure);
-    //divReceta.appendChild(pTituloDescripcion);
-    //divReceta.appendChild(pDescripcion);
     divReceta.appendChild(pTituloTiempo);
     divReceta.appendChild(pTiempo);
-    //divReceta.appendChild(pTituloIngredientes);*/
+
 
     let ingre = recetaJSON.INGREDIENTES;
     let ingreSolos = ingre.split(',');
 
-    /*for (let i = 0; i < ingreSolos.length; i++) {
-        let pIngredientes = crearElemento('p',null,'- '+ingreSolos[i]);
-        divReceta.appendChild(pIngredientes);
-    }*/
 
     return divReceta;
   }
